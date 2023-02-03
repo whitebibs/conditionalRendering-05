0fb8e18680e77126021bf83b05b764eaae3df89c
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-02-03 17:39:59 +0100

    Add files via upload

---
+++ src/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {Age} from "./Age"
+
+
+export class Welcome extends React.Component{
+render(){
+    return (
+    <div>
+    <p>Welcome, {this.props.name}</p>
+    <Age age={this.props.age}/>
+    </div>
+    )
+}
+}
+
+ 
